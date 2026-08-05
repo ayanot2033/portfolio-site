0834e6701fb75ebb0c1727d3744a320a6463f93a
ayanot2033 <ayano.t.2033@gmail.com>
2026-08-05 22:20:04 +0900

    fix: configure multi page build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,27 @@
 import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     base: "/portfolio-site/",
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                heal: resolve(__dirname, "pages/heal.html"),
+                yui: resolve(__dirname, "pages/yui.html"),
+                shin: resolve(__dirname, "pages/shin.html"),
+                sudachi: resolve(__dirname, "pages/sudachi.html"),
+                memoria: resolve(__dirname, "pages/memoria.html"),
+                beauty: resolve(__dirname, "pages/beauty.html"),
+                y2k: resolve(__dirname, "pages/y2k.html"),
+                "art-flag": resolve(__dirname, "pages/art-flag.html"),
+                geitaniand: resolve(__dirname, "pages/geitaniand.html"),
+                umigamenonamida: resolve(__dirname, "pages/umigamenonamida.html"),
+            },
+        },
+    },
 });
